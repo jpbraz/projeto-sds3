a3f5155333add422155ee8948ed2def0b76ed02b
João Paulo Araujo Braz <jparaujobraz84@gmail.com>
2021-05-06 11:04:31 -0300

    Layers

--- backend/src/main/java/com/devsuperior/dsvendas/dto/SellerDTO.java
+++ backend/src/main/java/com/devsuperior/dsvendas/dto/SellerDTO.java
@@ -1,10 +1,8 @@
 package com.devsuperior.dsvendas.dto;
 
-import java.io.Serializable;
 import com.devsuperior.dsvendas.entities.Seller;
 
-public class SellerDTO implements Serializable{
-	private static final long serialVersionUID = 1L;
+public class SellerDTO {
 	
 	private Long id;
 	private String name;
